af02c1bcad34593bf2bbecac1b303d5ce796d616
Adellia Jovina <111057926+AdelliaJovina31@users.noreply.github.com>
2024-11-18 17:25:34 +0700

    create header.js

---
+++ src/js/header.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarMenu = document.querySelectorAll('.navbar-menu');
+
+    navbarMenu.forEach((menu) => {
+        const menuText = menu.querySelector('a');
+
+        menu.addEventListener('mouseenter', () => {
+            menu.style.backgroundColor = '#00CC99';
+            menuText.style.color = 'white';
+        });
+
+        menu.addEventListener('mouseleave', () => {
+            menu.style.backgroundColor = 'transparent';
+            menuText.style.color = 'black';
+        });
+    });
+    
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const hamburgerMenuList = document.getElementById('not-pc-navbar');
+    const closeSidebar = document.getElementById('close-icon');
+    if (hamburgerMenu) {
+        hamburgerMenu.addEventListener('click', (e) => {
+            hamburgerMenuList.style.display = 'flex';
+        });
+
+        closeSidebar.addEventListener('click', (e) => {
+            hamburgerMenuList.style.display = 'none';
+        });
+    }
+
+    const sidebarMenu = document.querySelectorAll('.sidebar-menu');
+
+    sidebarMenu.forEach((menu) => {
+        const menuText = menu.querySelector('a');
+
+        menu.addEventListener('mouseenter', () => {
+            menuText.style.color = 'white';
+        });
+
+        menu.addEventListener('mouseleave', () => {
+            menuText.style.color = 'black';
+        });
+    });
+
+    const searchBar = document.getElementById('searchBar');
+    const newsList = document.querySelector('.news-list');
+    const newsItems = newsList.getElementsByClassName('news');
+
+    searchBar.addEventListener('input', function() {
+        const keyword = searchBar.value.toLocaleLowerCase();
+
+        // Array.from(newsItems).forEach(function(item) {
+        //     const title = item.querySelector('h4').textContent.toLocaleLowerCase();
+        //     const description = item.querySelector('p').textContent.toLocaleLowerCase();
+
+        //     // tampilkan berita kalau keyword ditemukan
+        //     if(title.includes(keyword) || description.includes(keyword)) {
+        //     item.style.display = '';
+        //     } else {
+        //     item.style.display = 'none';
+        //     }
+        // });
+    });
+});
